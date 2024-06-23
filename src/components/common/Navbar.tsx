e710a3a3e35449bc6616dfb7201e59eca1bbc098
jamesjoy-2k24 <jamesjoy2k24@gmail.com>
2024-06-23 23:28:28 +0530

    responsive navbar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,60 +1,102 @@
-import { NextPage } from 'next';
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { slideInFromTop } from '../../utils/motion';
-import { BsFacebook, BsInstagram, BsTwitterX, BsGithub } from 'react-icons/bs';
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsGithub,
+  BsList,
+  BsX,
+} from 'react-icons/bs';
 import Link from 'next/link';
 
-interface Props {}
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-const Navbar: NextPage<Props> = ({}) => {
+  const handleToggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>
-      <motion.div initial="hidden" animate="visible">
-        <div className="bg-transparent backdrop-blur-lg h-[70px] z-10 absolute text-gray-300 w-full flex justify-around items-center text-md">
-          <motion.div variants={slideInFromTop(0.3)} className="text-white">
-            <div>James Joy</div>
-          </motion.div>
-          <div>
-            <motion.div className="flex gap-12 ml-[5rem] bg-black bg-opacity-40 p-2 px-6 rounded-3xl">
-              <motion.div variants={slideInFromTop(0.4)}>
-                <Link className="hover:text-white" href={'/'}>
-                  Home
-                </Link>
-              </motion.div>
-              <motion.div variants={slideInFromTop(0.5)}>
-                <Link className="hover:text-white" href={'/'}>
-                  About
-                </Link>
-              </motion.div>
-              <motion.div variants={slideInFromTop(0.6)}>
-                <Link className="hover:text-white" href={'/'}>
-                  Contact
-                </Link>
-              </motion.div>
-              <motion.div variants={slideInFromTop(0.7)}>
-                <Link className="hover:text-white" href={'/'}>
-                  Projects
-                </Link>
-              </motion.div>
+    <nav className="fixed w-full z-50">
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        className="bg-transparent backdrop-blur-3xl h-[70px] text-gray-300 flex justify-between md:justify-around items-center md:px-10 px-6"
+      >
+        <motion.div
+          variants={slideInFromTop(0.3)}
+          className="text-white font-bold text-lg"
+        >
+          <div>James Joy</div>
+        </motion.div>
+
+        <div className="hidden md:flex gap-10 bg-black bg-opacity-50 ml-[5rem] font-bold p-2 px-6 rounded-3xl">
+          {['Home', 'About', 'Contact', 'Projects'].map((item, index) => (
+            <motion.div
+              key={index}
+              variants={slideInFromTop(0.4 + index * 0.1)}
+            >
+              <Link href={`/${item.toLowerCase()}`}>
+                <span className="hover:text-white cursor-pointer">{item}</span>
+              </Link>
             </motion.div>
-          </div>
-          <motion.div className="flex items-center gap-6 text-xl">
-            <motion.div variants={slideInFromTop(0.8)}>
-              <BsFacebook />
+          ))}
+        </div>
+
+        <motion.div className="hidden md:flex items-center gap-8 text-2xl">
+          {[BsFacebook, BsInstagram, BsTwitter, BsGithub].map((Icon, index) => (
+            <motion.div
+              key={index}
+              variants={slideInFromTop(0.8 + index * 0.1)}
+              whileHover={{
+                scale: 1.2,
+                color: '#faf3fb',
+              }}
+              transition={{ duration: 0.2 }}
+              className="cursor-pointer hover:drop-shadow-2xl hover:shadow-[#af7dff]"
+            >
+              <Icon />
             </motion.div>
-            <motion.div variants={slideInFromTop(0.9)}>
-              <BsInstagram />
-            </motion.div>
-            <motion.div variants={slideInFromTop(1)}>
-              <BsTwitterX />
-            </motion.div>
-            <motion.div variants={slideInFromTop(1.1)}>
-              <BsGithub />
-            </motion.div>
-          </motion.div>
+          ))}
+        </motion.div>
+
+        <div className="md:hidden flex items-center hover:rotate-45">
+          <button
+            onClick={handleToggleMenu}
+            className="text-3xl font-extrabold text-white hover:rotate-45"
+          >
+            {isOpen ? <BsX /> : <BsList />}
+          </button>
         </div>
       </motion.div>
-    </div>
+
+      {isOpen && (
+        <motion.div
+          initial={{ width: 0 }}
+          animate={{ width: '70%' }}
+          exit={{ width: 0 }}
+          className="fixed md:hidden top-0 right-0 h-full bg-black text-white flex flex-col items-center justify-center space-y-8 -z-50"
+        >
+          {['Home', 'About', 'Contact', 'Projects'].map((item, index) => (
+            <motion.div
+              key={index}
+              variants={slideInFromTop(0.4 + index * 0.1)}
+            >
+              <Link href={`/${item.toLowerCase()}`}>
+                <span
+                  onClick={handleToggleMenu}
+                  className="text-lg hover:text-white cursor-pointer"
+                >
+                  {item}
+                </span>
+              </Link>
+            </motion.div>
+          ))}
+        </motion.div>
+      )}
+    </nav>
   );
 };
 
